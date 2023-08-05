c006a38895fdb37ac526fa3f0b4daed8dd397106
migfoo02 <migfoo02@gmail.com>
2023-08-05 16:10:30 +0800

    added all functionalities with basic design

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
+const grid = document.querySelector('.grid-container');
+let gridValue = document.querySelector('.grid-size');
+let gridSize = document.querySelector('input');
+const resetBtn = document.querySelector('.reset');
+const applyGridSize = document.querySelector('.apply');
+let squareSize = 8;
 
-function generateGrid(size = 32 * 44, cssClass = 'medium-grid-default') {
-    const gameContainer = document.getElementById('grid-container');
-  
-    gameContainer.innerHTML = '';
-  
-    gameContainer.classList.remove('small-grid', 'medium-grid-default', 'big-grid');
-    gameContainer.classList.add(cssClass);
-  
-    for (let i = 0; i < size; i += 1) {
-      const div = document.createElement('div');
-      gameContainer.appendChild(div);
-    }
+createGrid(squareSize);
+
+function createDiv(size) {
+  const div = document.createElement('div');
+  div.classList.add('box');
+  div.style.width = `${size}px`;
+  div.style.height = `${size}px`;
+
+  return div;
+}
+
+function createGrid(gridSize) {
+  const gridArea = gridSize * gridSize;
+  for (let i = 0; i < gridArea; i++) {
+    grid.appendChild(createDiv(grid.clientWidth / gridSize));
   }
+}
 
-generateGrid();
+function reset() {
+  while (grid.firstChild) {
+    grid.removeChild(grid.lastChild);
+  }
+  createGrid(squareSize);
+}
+
+function shadeBox(e) {
+  if (e.target.matches('.box')) {
+    e.target.classList.add('active');
+  }
+}
+
+function changeGridSize(e) {
+  squareSize = e.target.value;
+  gridValue.textContent = `${squareSize}x${squareSize}`;
+}
+
+gridSize.addEventListener('input', changeGridSize);
+
+grid.addEventListener('mouseover', shadeBox);
+
+applyGridSize.addEventListener('click', reset)
+
+resetBtn.addEventListener('click', reset);
